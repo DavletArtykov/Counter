543cd242b75b1c8694ecdcc4df66b2ab12baa0ed
DavletArtykov <davletartykov6@gmail.com>
2022-04-26 10:31:01 +0600

    pracrice

--- src/App.js
+++ src/App.js
@@ -1,22 +1,58 @@
 
+
 import './App.css';
-import {Button} from './components/button/Button'
-import {Count} from './components/Count/Count'
+
+import { ExpenseItem } from './components/ExpenseItem';
+
+
+
+
 
 function App() {
 
-  
+  const expenses =[
+    {
+      id: '1',
+      title: 'Hello',
+      img: 'https://images.pexels.com/photos/9035242/pexels-photo-9035242.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500 ',
+      zag:'lorem',
+      side: 'https://www.youtube.com/'
 
+    },
+    {
+      id: '2',
+      title: 'Hi',
+      img: 'https://images.pexels.com/photos/8210512/pexels-photo-8210512.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500 ',
+      zag:'lorem',
+      side: 'https://www.google.com/'
 
+    },
+    {
+      id: '3',
+      title: 'My ',
+      img: ' https://images.pexels.com/photos/7544626/pexels-photo-7544626.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      zag:'lorem',
+      side: 'https://www.facebook.com/'
+
+    },
+    {
+      id: '4',
+      title: 'Hello',
+      img: 'https://images.pexels.com/photos/11866150/pexels-photo-11866150.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      zag:'lorem',
+      side: 'https://www.instagram.com/'
+
+    },
+
+  ]
+  console.log(expenses);
 
   return (
-    <div className="App">
-      <h1>Сounter</h1>
-       <Count counter ='0' />
-      <Button title='+'/>
-      <Button title='-'/>
+    <div className='App' >
+      <ExpenseItem expenses = {expenses} />
      
-    </div>
+
+     </div>
   );
 }
 
---
+++ src/components/ExpenseInput.jsx
@@ -0,0 +1,11 @@
+
+
+export const ExpenseInput = () =>{
+   
+    return(
+        <div className="container-input__wraper" >
+            <input type="text" />
+            <button  >Click</button>
+        </div>
+    )
+}
